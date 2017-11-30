2a58c9679cf82dd375162c1a43e067fe990efb9c
nnthao <nnt.ntc@hotmail.com.vn>
2017-11-30 21:03:13 +0700

    1
    1

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+module.exports = {
+    entry: [
+        './src/index.js',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server'
+    ],
+
+    output: {
+        filename: 'bundle.js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.svg$/,
+                loaders: [
+                    'babel-loader',
+                    {
+                        loader: 'react-svg-loader',
+                        query: {
+                            jsx: true
+                        }
+                    },
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+
+    devServer: {
+        contentBase: __dirname + "/public/",
+        inline: true,
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ]
+};
