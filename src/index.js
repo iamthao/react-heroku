03e6c2ea3522eaf8b97ff4ce7684c8012cd7bae6
nnthao <nnt.ntc@hotmail.com.vn>
2017-12-01 14:57:36 +0700

    1
    1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './src/index.css';
-import App from './src/App';
+import './index.css';
+//import App from './src/App';
 import registerServiceWorker from './registerServiceWorker';
 
 registerServiceWorker();
 module.hot.accept();
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render("test", document.getElementById('root'));
 
 
 
